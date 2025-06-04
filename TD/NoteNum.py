c2871a6d2e7c5dd208182afb2cbeca6da480d952
Kyle-coco <jjjjjdrew@gmail.com>
2025-06-04 13:17:20 +0800

    Update NoteNum.py

--- TD/NoteNum.py
+++ TD/NoteNum.py
@@ -10,19 +10,19 @@ classroom_member_df = pd.read_csv(classroom_member_path)
 
 print(classroom_member_df.head())
 
-# # 按班级 ID 划分，计算每个班级的笔记数和话题数的总和
-# classroom_summary = classroom_member_df.groupby('classroomId').agg({
-#     'noteNum': 'sum',
-#     'threadNum': 'sum'
-# }).reset_index()
-#
-# # 可视化柱状图
-# plt.figure(figsize=(10, 6))
-# plt.bar(classroom_summary['classroomId'], classroom_summary['noteNum'], label='笔记总数', color='skyblue')
-# plt.bar(classroom_summary['classroomId'], classroom_summary['threadNum'], bottom=classroom_summary['noteNum'], label='话题总数', color='orange')
-# plt.xlabel('班级ID')
-# plt.ylabel('数量')
-# plt.title('每个班级的笔记数和话题数总和')
-# plt.legend()
-# plt.grid(True)
-# plt.show()
+# 按班级 ID 划分，计算每个班级的笔记数和话题数的总和
+classroom_summary = classroom_member_df.groupby('classroomId').agg({
+    'noteNum': 'sum',
+    'threadNum': 'sum'
+}).reset_index()
+
+# 可视化柱状图
+plt.figure(figsize=(10, 6))
+plt.bar(classroom_summary['classroomId'], classroom_summary['noteNum'], label='笔记总数', color='skyblue')
+plt.bar(classroom_summary['classroomId'], classroom_summary['threadNum'], bottom=classroom_summary['noteNum'], label='话题总数', color='orange')
+plt.xlabel('班级ID')
+plt.ylabel('数量')
+plt.title('每个班级的笔记数和话题数总和')
+plt.legend()
+plt.grid(True)
+plt.show()
