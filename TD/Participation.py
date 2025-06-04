39c947b545f4a15b94397397252d35a0d64a2949
Kyle-coco <jjjjjdrew@gmail.com>
2025-06-04 13:18:08 +0800

    Update Participation.py

--- TD/Participation.py
+++ TD/Participation.py
@@ -1,13 +1,3 @@
-"""
-要统计学生整体参与学习平台程度的指标，我们可以利用 log 表中的 action 字段来创建一个“参与度指数”。这个指数将根据学生的活动（如登录、加入班级、下载等）的类型和频率来衡量。
-
-为了实现这一点，我们可以为不同类型的行为赋予不同的权重，这些权重反映了各种行为对于学生参与度的贡献大小。例如，我们可以认为加入课程或班级的行为比更改头像更能反映学生的参与度。
-
-以下是一种可能的方法来计算参与度指数，首先定义各种行为的权重，然后根据学生的行为频率和这些权重来计算每个学生的参与度指数：
-定义行为权重：为 log 表中的行为定义权重。
-计算参与度指数：对每个学生的行为使用权重进行加权，计算总分。
-"""
-
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -18,17 +8,7 @@ log_path = "D:/PyCharm/TD_data_analysis/game_TD/log.csv"
 classroom_member_df = pd.read_csv(classroom_member_path)
 log_df = pd.read_csv(log_path)
 
-# # 定义行为权重
-# action_weights = {
-#     'login_success': 1, 'join_classroom': 3, 'add_student': 2, 'download': 2,
-#     'create_course': 5, 'join_course': 4, 'publish_course': 4, 'update_course': 3,
-#     'create_lesson': 4, 'delete_course': -2, 'user_login': 1, 'user_logout': -1,
-#     'add_task': 3, 'delete_task': -2, 'create_thread': 3, 'update_member': 2,
-#     'delete_member': -2, 'register': 5, 'password-changed': 1
-#     # 可以继续添加其他行为和权重
-# }
-
-# 调整行为权重（减小为原来的四分之一）
+# 调整行为权重
 action_weights = {
     'login_success': 0.25, 'join_classroom': 0.75, 'add_student': 0.5, 'download': 0.5,
     'create_course': 1.25, 'join_course': 1, 'publish_course': 1, 'update_course': 0.75,
